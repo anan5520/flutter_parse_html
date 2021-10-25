11a45cb0f0d30b292376d5caba71f14d49924e36
anfeifan <feifan.an@skymed.ai>
2021-10-25 09:46:41 +0800

    1.3.28

--- android/app/src/main/java/com/arvin/flutter_parse_html/MyApplication.java
+++ android/app/src/main/java/com/arvin/flutter_parse_html/MyApplication.java
@@ -1,12 +1,5 @@
 package com.arvin.flutter_parse_html;
 
-import android.util.Log;
-
-import com.tencent.smtt.sdk.QbSdk;
-import com.umeng.commonsdk.UMConfigure;
-import com.umeng.message.IUmengRegisterCallback;
-import com.umeng.message.PushAgent;
-
 import io.flutter.app.FlutterApplication;
 
 public class MyApplication extends FlutterApplication {
